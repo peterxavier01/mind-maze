abcbe918a7c926e7341f5e53cf6a07f911073dba
Peter Uadiale <fxavy02@gmail.com>
2025-03-10 14:59:38 +0100

    refactor: add logic to conditionally render start and replay button based on the game status

--- src/components/controls.tsx
+++ src/components/controls.tsx
@@ -1,4 +1,4 @@
-import { Pause, Play } from "lucide-react";
+import { Forward, Pause, Play, RefreshCcw } from "lucide-react";
 
 import { ModeToggle } from "@/components/toggle-theme";
 import { Button } from "@/components/ui/button";
@@ -6,10 +6,13 @@ import BgMusicBtn from "@/components/bg-music-btn";
 import LevelSelector from "@/components/level-selector";
 
 import useGameStore from "@/store/use-game-store";
+import { useGoToNextLevel } from "@/hooks/use-go-to-next-level";
 
-const Controls = () => {
+const Controls = ({ resetLevel }: { resetLevel: () => void }) => {
   const toggleGameStatus = useGameStore((state) => state.toggleGameStatus);
   const status = useGameStore((state) => state.status);
+
+  const goToNextLevel = useGoToNextLevel();
 
   return (
     <div className="flex items-center justify-between mt-4">
@@ -19,18 +22,30 @@ const Controls = () => {
 
       <BgMusicBtn />
 
-      <Button
-        className="cursor-pointer"
-        onClick={toggleGameStatus}
-        disabled={status === "ended"}
-      >
-        {status === "running" ? <Pause /> : <Play />}
-        {status === "pending"
-          ? "Start"
-          : status === "running"
-            ? "Pause"
-            : "Play"}
-      </Button>
+      {status !== "ended" ? (
+        <Button className="cursor-pointer" onClick={toggleGameStatus}>
+          {status === "running" ? <Pause /> : <Play />}
+          {status === "pending"
+            ? "Start"
+            : status === "running"
+              ? "Pause"
+              : "Play"}
+        </Button>
+      ) : null}
+
+      {status === "ended" ? (
+        <Button className="cursor-pointer" onClick={resetLevel}>
+          <RefreshCcw />
+          Replay
+        </Button>
+      ) : null}
+
+      {status === "ended" ? (
+        <Button className="cursor-pointer" onClick={goToNextLevel}>
+          <Forward />
+          Next
+        </Button>
+      ) : null}
     </div>
   );
 };
