1820c0c5d788e298b76f91835c67984c7d7c4f5e
Peter Uadiale <fxavy02@gmail.com>
2025-03-10 14:58:38 +0100

    styles: modify component styles

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   const selectedImages = useCurrentLevelImages();
 
   return (
-    <main className="flex flex-col items-center justify-center max-w-xl mx-auto p-4 min-h-dvh">
+    <main className="flex flex-col items-center justify-center max-w-5xl w-full mx-auto p-4 min-h-dvh">
       {/** Adding a key prop helps re-render the component whenever there is a change in the currentLevelId
        * In our case, whenever the levelSelector component changes the game level, this component re-renders.
        * This is an alternative to the useEffect approach inside the MemoryCard component. setCards has its initial value provided in useState,
